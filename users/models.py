db7366a29d376594c2a972b9b7b6544f090865ec
PULK4N <nikola_s99@live.com>
2022-04-09 20:27:57 +0200

    profile updated

--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 from email.policy import default
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
 
@@ -10,3 +11,10 @@ class Profile(models.Model):
 
     def __str__(self) -> str:
         return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save()
+        img = Image.open(self.image.path)
+        if(img.height > 240 or img.width > 240):
+            img.thumbnail((240,240))
+            img.save(self.image.path)
